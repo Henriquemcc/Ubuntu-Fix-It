20ae879f5c47d249faa71b380cccb6e6e5876199
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-05-12 17:45:18 -0300

    Melhorando o script de correção de erros do bluetooth e do audio.

--- Fix_Audio.sh
+++ Fix_Audio.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo apt-get update;
-sudo apt-get dist-upgrade -y;
-sudo apt-get install pavucontrol linux-sound-base alsa-base alsa-utils lightdm ubuntu-desktop  linux-image-`uname -r` libasound2;
-sudo apt-get -y --reinstall install linux-sound-base alsa-base alsa-utils lightdm ubuntu-desktop  linux-image-`uname -r` libasound2;
-killall pulseaudio; rm -r ~/.pulse*;
+sudo apt update;
+sudo apt full-upgrade -y;
+sudo apt install --reinstall alsa-base alsa-utils alsa-tools-gui alsa-topology-conf alsa-ucm-conf bluedevil gir1.2-cvc-1.0 gir1.2-rb-3.0 gstreamer1.0-libav gstreamer1.0-nice gstreamer1.0-packagekit gstreamer1.0-pulseaudio indicator-sound libao-common libao4 libasound2-plugins libbasicusageenvironment1 libcanberra-pulse libkf5pulseaudioqt2 libpulse-mainloop-glib0 libpulse0 libpulsedsp lightdm linux-image-`uname -r` linux-sound-base pavucontrol pulseaudio pulseaudio-equalizer pulseaudio-module-bluetooth pulseaudio-module-jack pulseaudio-module-lirc pulseaudio-module-raop pulseaudio-module-zeroconf pulseaudio-utils python3-libdiscid speech-dispatcher-audio-plugins ubuntu-desktop ubuntu-sounds -y;
+sudo bash ./Fix_Bluetooth.sh;
+killall pulseaudio;
+rm -r ~/.pulse*;
 ubuntu-support-status;
 sudo usermod -aG `cat /etc/group | grep -e '^pulse:' -e '^audio:' -e '^pulse-access:' -e '^pulse-rt:' -e '^video:' | awk -F: '{print $1}' | tr '\n' ',' | sed 's:,$::g'` `whoami`;
-reboot;
+sudo alsa force-reload;
+#alsactl restore;
